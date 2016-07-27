c2c403261e6c50ab6dcf52187ee6286bcf22cf22
Diego Brengi <diego.brengi@gmail.com>
2016-07-27 02:33:21 -0300

    Agrego un script para reparar vistas 3D.

---
+++ scripts/3dfix1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Autor: Diego Brengi
+# Script rústico y peligroso para adaptar las vistas 3D. 
+# Usar bajo propia responsabilidad.
+# Pasar como argumento el archivo .kicad_pcb
+
+FILEIN=$1
+FILEOUT=3dfix_$1
+
+#Editar a mano en la siguiente línea el directorio donde estan los footprints
+
+cat $FILEIN | sed 's#model .*/\(.*\)\.wrl#model XXXYYY/\1.wrl#' | sed 's#XXXYYY#${KIPRJMOD}/footprints.3dshapes#' > $FILEOUT
+
